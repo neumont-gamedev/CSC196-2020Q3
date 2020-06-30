c1f4e20c24ad0638d9944f28f6ff67bb884e0085
Neumont College - Game Development <31079247+neumont-gamedev@users.noreply.github.com>
2020-06-30 15:39:37 -0600

    Engine Project Math Start

--- Game/main.cpp
+++ Game/main.cpp
@@ -1,19 +1,43 @@
 #include "core.h"
+#include "Math/Math.h"
+#include "Math/Vector2.h"
+#include "Math/Random.h"
 #include <iostream>
+
+const size_t NUM_POINTS = 40;
+std::vector<nc::Vector2> points;
 
 bool Update(float dt)
 {
+	for (size_t i = 0; i < NUM_POINTS; i++)
+	{
+		points[i] = nc::Vector2{ nc::random(0.0f, 800.0f), nc::random(0.0f, 600.0f) };
+	}
+
 	return false;
 }
 
 void Draw(Core::Graphics& graphics)
 {
 	graphics.SetColor(RGB(rand() % 256, rand() % 256, rand() % 256));
-	graphics.DrawLine(static_cast<float>(rand() % 800), static_cast<float>(rand() % 600), static_cast<float>(rand() % 800), static_cast<float>(rand() % 600));
+	graphics.DrawLine(nc::random(0.0f, 800.0f), nc::random(0.0f, 600.0f), nc::random(0.0f, 800.0f), nc::random(0.0f, 600.0f));
+
+	for (size_t i = 0; i < NUM_POINTS - 1; i+=2)
+	{
+		nc::Vector2 p1 = points[i];
+		nc::Vector2 p2 = points[i+1];
+
+		graphics.DrawLine(p1.x, p1.y, p2.x, p2.y);
+	}
 }
 
 int main()
 {
+	for (size_t i = 0; i < NUM_POINTS; i++)
+	{
+		points.push_back({ nc::random(0.0f, 800.0f), nc::random(0.0f, 600.0f) });
+	}
+
 	char name[] = "CSC196";
 	Core::Init(name, 800, 600);
 	Core::RegisterUpdateFn(Update);
